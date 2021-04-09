8918c86747bbee18e4c3e7126abe43da78203525
asparagoose2 <ofir.duchovne@gmail.com>
2021-04-09 23:16:38 +0300

    EX3: done

--- exercises/ex3/js/scripts.js
+++ exercises/ex3/js/scripts.js
@@ -1,5 +1,5 @@
 // var colors = ["#012531", "#0C5B70", "#1598C0" , "#41A2CF", "#90D2E8"];
-var colors = ["#E63946", "#E96A70", "#EC9A9A", "#EFCAC4", "#F1FAEE", "#A8DADC", "#77ABBD", "#457B9D", "#31587A", "#1D3557"];
+var colors = ["#E63946", "#E96A70", "#EC9A9A", "#EFCAC4", "#92c084", "#A8DADC", "#77ABBD", "#457B9D", "#31587A", "#1D3557"];
 // var colors = ["2,63,165","125,135,185","190,193,212","214,188,192","187,119,132","142,6,59","74,111,227","133,149,225","181,187,227","230,175,185","224,123,145","211,63,106","17,198,56","141,213,147","198,222,199","234,211,198","240,185,141","239,151,8","15,207,192","156,222,214","213,234,231","243,225,235","246,196,225","247,156,212"];
 var rectangles = [];
 var lastName = "duchovne";
@@ -16,13 +16,23 @@ makeRectangles = function() {
     for(i in lastName)
     {
         makeRectangle();
+        makeRectangle();
     }
-    console.log(rectangles);
 }
 
 toggleColor = function () {
     var index = (this.id.toString()).substring(3);
-    this.style.background = rectangles[index]["isToggled"]? rectangles[index]["color"] : "white";
+
+    if(rectangles[index]["isToggled"])
+    {
+        this.style.background = rectangles[index]["color"];
+        this.style.backgroundImage = "none";
+    } else {
+        this.style.background = "white";
+        this.style.backgroundImage = 'url("/images/success-kid.jpg")';
+        this.style.backgroundRepeat = "no-repeat";
+        this.style.backgroundPosition = "center";
+    }
     rectangles[index]["isToggled"] = !rectangles[index]["isToggled"];
 }
 
@@ -30,11 +40,19 @@ makeRecElement = function(rec) {
     var x = document.createElement("div");
     x.id = rectangles[rec]["id"];
     x.className = "layout3rec";
-    x.innerHTML = rectangles[rec]["color"];
+    // x.innerHTML = rectangles[rec]["color"];
     x.style.background = rectangles[rec]["color"];
     x.onclick = toggleColor;
+    if(!((rec+2)%3))
+    {
+        var star = document.createElement("div");
+        star.className = "star";
+        x.appendChild(star);
+        // x.children += star;
+    }
     return x;
 }
+
 
 addRectangle = function() {
     makeRectangle();
