328f600b78705994fd2b425927d16d774f0d3b99
asparagoose2 <ofir.duchovne@gmail.com>
2021-04-25 16:26:56 +0300

    EX4: merging all of the tests to one finalized product

--- exercises/ex4/js/scripts.js
+++ exercises/ex4/js/scripts.js
@@ -1,3 +1,151 @@
+
+var sdegree = 0;
+var pos = 2000;
+var ang = 40;
+			
+		var doc = document;
+
+moveText = function(possition) {
+  let delta_y = Math.sin(Math.PI * ang / 180) * possition;
+      $('.scroll-text').css({
+          'transform': 'rotateX(' + ang + 'deg) translateZ(' + 1800 + 'px) translateY(' + delta_y + 'px)' 
+      });
+}
+
+		if (doc.addEventListener) {
+			// IE9, Chrome, Safari, Opera
+			doc.addEventListener("mousewheel", MouseWheelHandler, false);
+			// Firefox
+			doc.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+		}
+		// IE 6/7/8
+		else doc.attachEvent("onmousewheel", MouseWheelHandler);
+    
+    moveText(pos);
+    document.getElementById("muteBtn").onclick = function() {
+      
+      if(!document.getElementsByTagName("audio")[0].paused) {
+        this.src = "../images/mute.svg";
+        document.getElementsByTagName("audio")[0].pause();
+      } else {
+        this.src = "../images/soundOn.svg";
+        document.getElementsByTagName("audio")[0].play();
+      }
+    };
+    document.getElementsByClassName("startBtn")[0].onclick = function() {
+    this.style.visibility = "hidden";
+    document.getElementsByClassName("introLine")[0].style.animationPlayState = "running";
+    document.getElementsByClassName("logo")[0].style.visibility = "visible";
+    document.getElementsByClassName("logo")[0].style.animationPlayState = "running";
+      document.getElementsByTagName("audio")[0].play();
+
+    setTimeout(() => {
+      document.getElementsByClassName("scroll-text")[0].style.visibility = "visible";
+      var id = setInterval(() => {
+        moveText(pos);
+        pos = pos-1;
+        if (pos <= 35) {
+          clearInterval(id);
+        }
+      }, 10);
+      document.getElementById("muteBtn").style.visibility = "visible";
+    }, 14000);
+  }
+		
+		function MouseWheelHandler(e) {
+			// cross-browser wheel delta
+			var e = window.event || e; // old IE support
+			var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+			pos += delta*25;
+			sdegree += 0.5 * delta;
+
+      moveText(pos);
+				
+			// var o = animateText(sdegree);
+			
+			// $('.scroll-text').css({
+			// 	'top': o.top + 'px', 
+			// 	'transform': 'rotateX(' + o.rotateX + 'deg) translateZ(' + o.translateZ + 'px)'
+			// });
+
+      
+      // let delta_y = Math.sin(Math.PI * ang / 180) * pos;
+      // $('.scroll-text').css({
+      //     'transform': 'rotateX(' + ang + 'deg) translateZ(' + 1800 + 'px) translateY(' + delta_y + 'px)' 
+      // });
+			
+			return false;
+		}
+
+// function animateText(step){
+		
+//   var limit = {'min': 0, 'max': 100};
+
+//   var top_l = {'min': 0, 'max': -6000};
+//   var rotateX_l = {'min': 20, 'max': 21};
+//   var translateZ_l = {'min': 0, 'max': -2500};
+  
+//   //
+
+//   var m_top = (top_l.max - top_l.min) / (limit.max - limit.min);
+//   var m_rotateX = (rotateX_l.max - rotateX_l.min) / (limit.max - limit.min);
+//   var m_translateZ = (translateZ_l.max - translateZ_l.min) / (limit.max - limit.min);
+    
+//   var top = m_top * (step) + top_l.min;
+//   var rotateX = m_rotateX * (step) + rotateX_l.min; 
+//   var translateZ = m_translateZ * (step) + translateZ_l.min;
+  
+//   var output = {
+//     'top': top,
+//     'rotateX': rotateX, 
+//     'translateZ': translateZ
+//   };
+  
+//   return output;
+  
+// }
+
+
+
+
+
+$.fn.textWidth = function(text, font) {
+    
+  if (!$.fn.textWidth.fakeEl) $.fn.textWidth.fakeEl = $('<span>').hide().appendTo(document.body);
+  
+  $.fn.textWidth.fakeEl.text(text || this.val() || this.text() || this.attr('placeholder')).css('font', font || this.css('font'));
+  
+  return $.fn.textWidth.fakeEl.width();
+};
+
+$('.starWarsInput').on('input', function() {
+  var inputWidth = $(this).textWidth();
+  $(this).css({
+      width: (inputWidth + 10)
+  })
+}).trigger('input');
+
+
+function inputWidth(elem, minW, maxW) {
+  elem = $(this);
+  console.log(elem)
+}
+
+var targetElem = $('.starWarsInput');
+
+inputWidth(targetElem);
+
+
+
+
+
+
+
+
+
+
+
+
 var windowHeight = $('#intro').height();
 var windowWidth = $('#intro').width();
 
